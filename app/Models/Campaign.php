de93fa232162b35869d73f99603e0399b2359434
alerioreyhan <alerio.reyhan@mceasy.co.id>
2023-04-17 20:17:26 +0700

    add seeder, migration, and fillables

--- app/Models/Campaign.php
+++ app/Models/Campaign.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Campaign extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'desc',
+        'author_id',
+        'start_date',
+        'dateline',
+        'target_amount',
+        'img_url',
+        'status'
+    ];
 }
